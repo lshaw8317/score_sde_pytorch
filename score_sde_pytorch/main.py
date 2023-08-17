6adf89b1392e27a3567777efe5b2615e5823d6a2
Luke <lshaw8317@gmail.com>
2023-08-17 11:47:51 +0100

    Changed MLMC algorithm to use precalculated alpha and beta from initial Nsamples run in Gilesplot

--- score_sde_pytorch/main.py
+++ score_sde_pytorch/main.py
@@ -31,7 +31,7 @@ FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file(
   "config", None, "Training configuration.", lock_config=True)
 flags.DEFINE_string("workdir", None, "Work directory.")
-flags.DEFINE_enum("mode", None, ["train", "eval"], "Running mode: train or eval")
+flags.DEFINE_enum("mode", None, ["MLMC", "MC"], "Running mode: MLMC or MC")
 flags.DEFINE_string("eval_folder",None,"The folder name for storing evaluation results")
 flags.DEFINE_enum("payoff",'images',['images','activations','variance'],"Payoff functions for MLMC")
 flags.DEFINE_list("acc",[0.1,.05,.01,.005,.001,.0005,.0001],"Accuracies for MLMC")
@@ -39,24 +39,12 @@ flags.DEFINE_enum('MLMCsampler','EM',['EM','DDIM'],"Sampler to use for MLMC")
 flags.mark_flags_as_required(["workdir", "config", "mode","eval_folder"])
 
 def main(argv):
-  if FLAGS.mode == "train":
-    # Create the working directory
-    tf.io.gfile.makedirs(FLAGS.workdir)
-    # Set logger so that it outputs to both console and file
-    # Make logging work for both disk and Google Cloud Storage
-    gfile_stream = open(os.path.join(FLAGS.workdir, 'stdout.txt'), 'w')
-    handler = logging.StreamHandler(gfile_stream)
-    formatter = logging.Formatter('%(levelname)s - %(filename)s - %(asctime)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger = logging.getLogger()
-    logger.addHandler(handler)
-    logger.setLevel('INFO')
-    # Run the training pipeline
-    run_lib.train(FLAGS.config, FLAGS.workdir)
-  elif FLAGS.mode == "eval":
+  print(f'DDIM eta={FLAGS.config.mlmc.DDIM_eta}')
+  if FLAGS.mode == "MLMC":
     # Run the evaluation pipeline
-    print(f'DDIM eta={FLAGS.config.mlmc.DDIM_eta}')
-    MLMC.mlmc_test(FLAGS.config,FLAGS.eval_folder,FLAGS.workdir,FLAGS.payoff,[float(a) for a in FLAGS.acc],FLAGS.MLMCsampler)
+    MLMC.mlmc_test(FLAGS.config,FLAGS.eval_folder,FLAGS.workdir,FLAGS.payoff,[float(a) for a in FLAGS.acc],FLAGS.MLMCsampler,MLMC_=True)
+  elif FLAGS.mode == "MC":
+    MLMC.mlmc_test(FLAGS.config,FLAGS.eval_folder,FLAGS.workdir,FLAGS.payoff,[float(a) for a in FLAGS.acc],FLAGS.MLMCsampler,MLMC_=False)
   else:
     raise ValueError(f"Mode {FLAGS.mode} not recognized.")
 
